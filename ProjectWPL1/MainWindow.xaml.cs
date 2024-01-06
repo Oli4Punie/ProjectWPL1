064055d35dbf70e71a590f4623d1898e437d9367
Oli4Punie <11801972@student.pxl.be>
2024-01-06 17:09:30 +0100

    Groote getallen aangemaakt

--- ProjectWPL1/MainWindow.xaml.cs
+++ ProjectWPL1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -31,11 +32,13 @@ namespace ProjectWPL1
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick; 
             timer.Start();
+            DispatcherTimer passiefInkomen = new DispatcherTimer();
+            passiefInkomen.Interval = TimeSpan.FromMilliseconds(10);
+            passiefInkomen.Tick += passiefInkomen_Tick;
+            passiefInkomen.Start();
         }
         int elapsedSeconds = 0;
-        int cookies = 0;
-        bool isMouseDown = false;
-        double multiplier = 1;
+        long cookies = 0;
         int AantalCursors = 0;
         int AantalGrandma = 0;
         int AantalFarm = 0;
@@ -50,47 +53,48 @@ namespace ProjectWPL1
         int AantalKeerGekochtFactory = 0;
         int AantalKeerGekochtBank = 0;
         int AantalKeerGekochtTemple = 0;
+        int coockiesPerSeconde = 0;
         private void timer_Tick(object sender, EventArgs e)
         {
             elapsedSeconds++;
             LblTimer.Content = $"Verstreken tijd: {elapsedSeconds}";
         }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void passiefInkomen_Tick(object sender, EventArgs e)
         {
-
-        }
-        private void BtnCoockie_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
-        private void BtnCoockie_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-        private void ImgCoockie_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (isMouseDown)
-            {
-                ImgCoockie.Width = 250;
-                ImgCoockie.Height = 250;
-            }
-            isMouseDown = false;
-        }
-        private void ImgCoockie_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            ImgCoockie.Width = 180;
-            ImgCoockie.Height = 180;
-            isMouseDown = true;
+            double cursorPassiefInkomenPerInvestering = 0.001;
+            int totaalCursor = AantalCursors;
+            double grandmaPassiefInkomenPerInvestering = 0.01;
+            int totaalGrandma = AantalGrandma;
+            double farmPassiefInkomenPerInvestring = 0.08;
+            int totaalFarm = AantalFarm;
+            double minePassiefInkomenPerInvestering = 0.47;
+            int totaalMine = AantalMine;
+            double factoryPassiefInkomenPerInvestering = 2.60;
+            int totaalFactory = AantalFactory;
+            double bankPassiefInkomenPerInvestering = 14;
+            int totaalBank = AantalBank;
+            double templePassiefInkomenPerInvestering = 78;
+            int totaalTemple = AantalTemple;
+            double totaalPassiefInkomenCursor = cursorPassiefInkomenPerInvestering * totaalCursor;
+            double totaalPassiefInkomenGrandma = grandmaPassiefInkomenPerInvestering * totaalGrandma;
+            double totaalPassiefInkomenFarm = farmPassiefInkomenPerInvestring * totaalFarm;
+            double totaalPassiefInkomenMine = minePassiefInkomenPerInvestering * totaalMine;
+            double totaalPassiefInkomenFactory = factoryPassiefInkomenPerInvestering * totaalFactory;
+            double totaalPassiefInkomenBank = bankPassiefInkomenPerInvestering * totaalBank;
+            double totaalPassiefInkomenTemple = templePassiefInkomenPerInvestering * totaalTemple;
+            cookies += (int)(totaalPassiefInkomenCursor + totaalPassiefInkomenGrandma + totaalPassiefInkomenFarm + totaalPassiefInkomenMine + totaalPassiefInkomenFactory + totaalPassiefInkomenBank + totaalPassiefInkomenTemple);
+            coockiesPerSeconde = (int)(totaalPassiefInkomenCursor + totaalPassiefInkomenGrandma + totaalPassiefInkomenFarm + totaalPassiefInkomenMine + totaalPassiefInkomenFactory + totaalPassiefInkomenBank + totaalPassiefInkomenTemple);
+            UpdateCookieCount();
+            GemiddeldeCoockies(coockiesPerSeconde);
         }
         private void BtnCoockie_Click(object sender, RoutedEventArgs e)
         {
-            cookies++;
+            cookies+= 100000;
             UpdateCookieCount();
         }
         private void UpdateCookieCount()
         {
-            LblCookieCount.Content = cookies + "Coockies";
+            LblCookieCount.Content = GrooteNummers(cookies) + "Coockies";
         }
         private void BtnCursor_Click(object sender, RoutedEventArgs e)
         {
@@ -106,12 +110,13 @@ namespace ProjectWPL1
                 nieuweKosten = Math.Ceiling(nieuweKosten);
                 LblPrijsCursor.Content = $"Prijs: {nieuweKosten} cookies";
                 UpdateCursorCount();
-                UpdateCookieCount();
+                UpdateCookieCount(); 
             }
             else
             {
                 MessageBox.Show("Je hebt niet genoeg cookies om dit te kopen!", "Waarschuwing");
             }
+            
         }
         private void UpdateCursorCount()
         {
@@ -269,10 +274,40 @@ namespace ProjectWPL1
         {
             LblAantalGekochteUpgradesTemple.Content = $"Aantal temple's {AantalTemple}";
         }
-        private void GemiddeldeCoockies()
+        private void GemiddeldeCoockies(double cps)
         {
-
-            //LblGemCoockies.Content = $"Gemiddelde coockies: {}"; 
+            LblGemCoockies.Content = $"{cps:F1} Coockies per seconde";
+        }
+        private string GrooteNummers(long nummmer)
+        { 
+            const double Miljard = 1_000_000_000;
+            const double Billion = 1_000_000_000_000;
+            const double Biljart = 1_000_000_000_000_000;
+            const double Trilion = 1_000_000_000_000_000_000;
+            if (nummmer >= Trilion)
+            {
+                return $"{nummmer / Trilion:F3} Triljoen";
+            }
+            else if (nummmer >= Biljart)
+            {
+                return $"{nummmer / Biljart:F3} Biljart";
+            }
+            else if(nummmer >= Billion)
+            {
+                return $"{nummmer / Billion:F3} Biljoen";
+            }
+            else if (nummmer >= Miljard)
+            {
+                return $"{nummmer / Miljard} Miljard";
+            }
+            else if (nummmer >= 1_000_000)
+            {
+                return $"{nummmer / 1_000_000:F3} Miljoen";
+            }
+            else
+            {
+                return $"{nummmer:N0}";
+            }
         }
         private void LblNaamMiner_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
